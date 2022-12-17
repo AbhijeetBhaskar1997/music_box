9afc5e9fa4c9d106846b83077edbeb8ceff424be
Abhijeet Bhaskar <111655231+AbhijeetBhaskar1997@users.noreply.github.com>
2022-12-17 22:22:53 +0530

    Add files via upload

---
+++ music_box.py
@@ -0,0 +1,13 @@
+#Playing a song using playsound module.
+
+# import required module
+from playsound import playsound
+ 
+# for playing note.wav file
+playsound('https://cdn.pixabay.com/download/audio/2021/10/28/audio_5516a1d606.mp3?filename=piano-cassical-brand-motive-logo-9997.mp3')
+playsound('https://cdn.pixabay.com/download/audio/2022/11/24/audio_254450143d.mp3?filename=intro-logo-far_away_sting-127007.mp3')
+playsound('https://cdn.pixabay.com/download/audio/2022/01/03/audio_0b85b506ae.mp3?filename=good-morning-13396.mp3')
+playsound('https://cdn.pixabay.com/download/audio/2022/10/23/audio_274d153a55.mp3?filename=happy-123735.mp3')
+playsound('https://cdn.pixabay.com/download/audio/2021/12/29/audio_0c189dbbe9.mp3?filename=cartoon-intro-13087.mp3')
+
+
